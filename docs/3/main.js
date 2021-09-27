dd902fe799ec03d9d91f629ebb4d27004b3dcbd9
ytyaru <yttry0@gmail.com>
2021-09-27 17:55:25 +0900

    set md url

--- docs/3/main.js
+++ docs/3/main.js
@@ -3,6 +3,6 @@ window.addEventListener('load', (event) => {
     copyQuote(
         dom_sel = 'body', 
         onNotification=()=>{iziToast.success({title: '引用HTMLをコピーしました', message: 'あなたのサイトにペーストしてください'});});
-    parseMarkdown(md_url='/3/index.md');
+    parseMarkdown(md_url='/JS.MakeQuoteHtml.ParseMarkdown.20210926092329/3/index.md');
 });
 
