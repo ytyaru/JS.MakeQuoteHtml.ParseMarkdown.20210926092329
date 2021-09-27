a8a208225c8d4eb72244c2a75b6686157d8234f4
ytyaru <yttry0@gmail.com>
2021-09-27 18:06:01 +0900

    function

--- docs/3/main.js
+++ docs/3/main.js
@@ -2,7 +2,7 @@ window.addEventListener('load', (event) => {
     setupToast();
     copyQuote(
         dom_sel = 'body', 
-        onNotification=()=>{
+        onNotification=function(){
             console.log('引用HTMLをコピーしました');
             iziToast.success({title: '引用HTMLをコピーしました', message: 'あなたのサイトにペーストしてください'});
         }
