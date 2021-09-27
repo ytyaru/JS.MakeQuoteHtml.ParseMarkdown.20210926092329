2ab78b7bd6dbe149362115275f4de93e05e2ec1b
ytyaru <yttry0@gmail.com>
2021-09-27 18:02:46 +0900

    test

--- docs/3/main.js
+++ docs/3/main.js
@@ -2,7 +2,11 @@ window.addEventListener('load', (event) => {
     setupToast();
     copyQuote(
         dom_sel = 'body', 
-        onNotification=()=>{iziToast.success({title: '引用HTMLをコピーしました', message: 'あなたのサイトにペーストしてください'});});
+        onNotification=()=>{
+            console.log('引用HTMLをコピーしました');
+            iziToast.success({title: '引用HTMLをコピーしました', message: 'あなたのサイトにペーストしてください'});
+        }
+    );
     parseMarkdown(md_url='/JS.MakeQuoteHtml.ParseMarkdown.20210926092329/3/index.md');
 });
 
