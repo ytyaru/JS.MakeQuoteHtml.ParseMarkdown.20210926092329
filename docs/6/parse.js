b49ab718337678b109a28c78a21ea75770fb6832
ytyaru <ytyaru@outlook.jp>
2021-09-28 13:23:07 +0900

    clean

--- docs/6/parse.js
+++ docs/6/parse.js
@@ -1,5 +1,4 @@
 function parseMarkdown(url = '/content.md', selector = 'body', onParse = null) {
-//function parseMarkdown(url = '/content.md', selector = 'body', title_format = null, is_log = false) {
     fetch(url)
     .then(response => response.text())
     .then(data => {
@@ -7,15 +6,6 @@ function parseMarkdown(url = '/content.md', selector = 'body', onParse = null) {
             html_txt = markdown.parse(data)
             document.querySelector(selector).innerHTML = html_txt
             if (null != onParse) { onParse(html_txt); }
-            /*
-            if (is_log) { console.log(html_txt) }
-            if (title_format != null) {
-                h1 = document.querySelector(`${selector} h1`)
-                title = title_format.replace(/{{h1}}/g, h1.textContent)
-                document.querySelector('title').innerHTML = title
-                if (is_log) { console.log(`タイトルを置換しました：${title}`) }
-            }
-            */
         })
     });
 }
