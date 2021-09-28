27a29f922292842e9c2c513416077493ee29b290
ytyaru <ytyaru@outlook.jp>
2021-09-28 12:39:08 +0900

    bugfix h1 = document.querySelector(`${selector} h1`)

--- docs/5/parse.js
+++ docs/5/parse.js
@@ -7,7 +7,7 @@ function parseMarkdown(url = '/content.md', selector = 'body', title_format = nu
             document.querySelector(selector).innerHTML = html_txt
             if (is_log) { console.log(html_txt) }
             if (title_format != null) {
-                h1 = document.querySelector(`{selector} h1`)
+                h1 = document.querySelector(`${selector} h1`)
                 title = title_format.replace(/{{h1}}/g, h1.textContent)
                 document.querySelector('title').innerHTML = title
                 if (is_log) { console.log(`タイトルを置換しました：${title}`) }
